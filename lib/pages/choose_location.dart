81c33d4fb3d25b67b879b67602c047644179c040
Aaron Kurian <aaronkurianabraham@gmail.com>
2025-08-30 20:31:23 +0530

    feat: Created a counter to state init and build state

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,8 +8,19 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  } 
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
       backgroundColor: Colors.blueGrey[200],
       appBar: AppBar(
@@ -23,7 +34,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('Choose a location')
+      body: ElevatedButton(
+        onPressed: () {
+        setState(() {
+          counter+=1;
+        });
+      },
+        child: Text('Counter is $counter '),
+      ),
     );
   }
 }
