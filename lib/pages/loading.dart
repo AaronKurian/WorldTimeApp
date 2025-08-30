0135c242a0df01a3f65916666517502b84d0d58e
Aaron Kurian <aaronkurianabraham@gmail.com>
2025-08-30 20:09:22 +0530

    feat: Created basic pages

---
+++ lib/pages/loading.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoadingState extends StatefulWidget {
+  const LoadingState({super.key});
+
+  @override
+  State<LoadingState> createState() => _LoadingStateState();
+}
+
+class _LoadingStateState extends State<LoadingState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
